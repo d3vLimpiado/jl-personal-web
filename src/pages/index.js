cb916b490996fb6204a5636ec4217593efc2fd18
d3vlimpiado <jianlimpiado@gmail.com>
2021-10-06 20:18:57 +0800

    Add react helmet
    Added the title elements and some meta tags using react-helmet.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,24 @@
-import React from "react";
-import { Greet, AboutMe, Projects, Contacts, Navi } from "../components/index";
-import { GlobalStyle, MainWrapper, ContentWrapper } from "../style/index";
+import React from "react"
+import { Helmet } from "react-helmet"
+import { Greet, AboutMe, Projects, Contacts, Navi } from "../components/index"
+import { GlobalStyle, MainWrapper, ContentWrapper } from "../style/index"
 
 export default function Index() {
     return (
         <div>
             <GlobalStyle />
+            <Helmet>
+                <meta
+                    name="google-site-verification"
+                    content="Sp8xegZdm7bpV1Gm6--joN94BhogHOt0hsujTRpHdxE"
+                />
+                <meta property='og:url' content='https://d3vlimpiado.netlify.app' />
+                <meta property='og:title' content='JOSHUA LIMPIADO' />
+                <meta property='og:description' content='Personal Website Project built and designed with Reactjs as GatsbyJS as its framework.' />
+                <title>JOSHUA LIMPIADO</title>
+            </Helmet>
             <MainWrapper>
-                <Navi/>
+                <Navi />
                 <ContentWrapper>
                     <Greet />
                     <AboutMe />
@@ -16,5 +27,5 @@ export default function Index() {
                 </ContentWrapper>
             </MainWrapper>
         </div>
-    );
+    )
 }
